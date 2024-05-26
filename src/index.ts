6a1613125bca8f2dccbb44f8d45c3e3626902ac3
TejasGaikwad07 <tejasbharatgaikwad@gmail.com>
2024-05-26 21:15:19 +0530

    frmatted the code

--- src/index.ts
+++ src/index.ts
@@ -5,29 +5,28 @@ import client from "prom-client";
 const app = express();
 
 app.use(express.json());
-app.use(middleware)
-app.use(requestCountMiddleware)
-
+app.use(middleware);
+app.use(requestCountMiddleware);
 
 app.get("/user", (req, res) => {
-    res.send({
-        name: "John Doe",
-        age: 25,
-    });
+  res.send({
+    name: "John Doe",
+    age: 25,
+  });
 });
 
 app.post("/user", (req, res) => {
-    const user = req.body;
-    res.send({
-        ...user,
-        id: 1,
-    });
+  const user = req.body;
+  res.send({
+    ...user,
+    id: 1,
+  });
 });
 
 app.get("/metrics", async (req, res) => {
-    const metrics = await client.register.metrics();
-    res.set('Content-Type', client.register.contentType);
-    res.end(metrics);
-})
+  const metrics = await client.register.metrics();
+  res.set("Content-Type", client.register.contentType);
+  res.end(metrics);
+});
 
 app.listen(3000);
