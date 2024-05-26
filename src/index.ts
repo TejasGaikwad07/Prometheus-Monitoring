e8ad816826f2bdb41c910cab0ad9fccec0f154af
TejasGaikwad07 <tejasbharatgaikwad@gmail.com>
2024-05-26 21:13:06 +0530

    exposed the metrics endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import { middleware } from "./Monitoring/middleware";
-
+import { requestCountMiddleware } from "./metrics/requestCount";
+import client from "prom-client";
 const app = express();
 
 app.use(express.json());
 app.use(middleware)
+app.use(requestCountMiddleware)
+
 
 app.get("/user", (req, res) => {
     res.send({
@@ -20,5 +23,11 @@ app.post("/user", (req, res) => {
         id: 1,
     });
 });
+
+app.get("/metrics", async (req, res) => {
+    const metrics = await client.register.metrics();
+    res.set('Content-Type', client.register.contentType);
+    res.end(metrics);
+})
 
 app.listen(3000);
