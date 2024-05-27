0101cebabecaf5b3993b75de3cbacdcaa41a33ed
TejasGaikwad07 <tejasbharatgaikwad@gmail.com>
2024-05-27 12:51:38 +0530

    adding client for a histogram

--- src/Monitoring/requestCount.ts
+++ src/Monitoring/requestCount.ts
@@ -12,6 +12,13 @@ export const activeRequestsGauge = new client.Gauge({
     help: 'Number of active requests'
 });
 
+export const httpRequestDurationMicroseconds = new client.Histogram({
+    name: 'http_request_duration_ms',
+    help: 'Duration of HTTP requests in ms',
+    labelNames: ['method', 'route', 'code'],
+    buckets: [0.1, 5, 15, 50, 100, 300, 500, 1000, 3000, 5000] 
+});
+
 export const cleanupMiddleware = (req: Request, res: Response, next: NextFunction) => {
     const startTime = Date.now();
     activeRequestsGauge.inc();
