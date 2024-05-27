275f220cb5af1da29b9a5ba6f9f28ed76dc456eb
TejasGaikwad07 <tejasbharatgaikwad@gmail.com>
2024-05-27 12:37:17 +0530

    formatting the code

--- src/Monitoring/requestCount.ts
+++ src/Monitoring/requestCount.ts
@@ -1,27 +1,29 @@
 import { NextFunction, Request, Response } from "express";
 import client from "prom-client";
 
-
 const requestCounter = new client.Counter({
-    name: 'http_requests_total',
-    help: 'Total number of HTTP requests',
-    labelNames: ['method', 'route', 'status_code']
+  name: "http_requests_total",
+  help: "Total number of HTTP requests",
+  labelNames: ["method", "route", "status_code"],
 });
 
-export const requestCountMiddleware = (req: Request, res: Response, next: NextFunction) => {
-    const startTime = Date.now();
+export const requestCountMiddleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const startTime = Date.now();
 
-    res.on('finish', () => {
-        const endTime = Date.now();
-        console.log(`Request took ${endTime - startTime}ms`);
+  res.on("finish", () => {
+    const endTime = Date.now();
+    console.log(`Request took ${endTime - startTime}ms`);
 
-      
-        requestCounter.inc({
-            method: req.method,
-            route: req.route ? req.route.path : req.path,
-            status_code: res.statusCode
-        });
+    requestCounter.inc({
+      method: req.method,
+      route: req.route ? req.route.path : req.path,
+      status_code: res.statusCode,
     });
+  });
 
-    next();
+  next();
 };
