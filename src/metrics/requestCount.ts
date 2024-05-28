43fada5b5bb7cf257b3e1198466cc7d629d210b0
TejasGaikwad07 <tejasbharatgaikwad@gmail.com>
2024-05-28 20:42:12 +0530

    adding structure

--- src/metrics/requestCount.ts
+++ src/metrics/requestCount.ts
@@ -1,61 +1,9 @@
 import { NextFunction, Request, Response } from "express";
 import client from "prom-client";
 
-const requestCounter = new client.Counter({
-  name: "http_requests_total",
-  help: "Total number of HTTP requests",
-  labelNames: ["method", "route", "status_code"],
+// Create a counter metric
+export const requestCounter = new client.Counter({
+    name: 'http_requests_total',
+    help: 'Total number of HTTP requests',
+    labelNames: ['method', 'route', 'status_code']
 });
-
-const activeRequestsGauge = new client.Gauge({
-    name: 'active_requests',
-    help: 'Number of active requests'
-});
-
-const httpRequestDurationMicroseconds = new client.Histogram({
-    name: 'http_request_duration_ms',
-    help: 'Duration of HTTP requests in ms',
-    labelNames: ['method', 'route', 'code'],
-    buckets: [0.1, 5, 15, 50, 100, 300, 500, 1000, 3000, 5000] 
-});
-
-export const requestCountMiddleware = (
-    req: Request,
-    res: Response,
-    next: NextFunction
-  ) => {
-    const startTime = Date.now();
-  
-    res.on("finish", () => {
-      const endTime = Date.now();
-      console.log(`Request took ${endTime - startTime}ms`);
-          httpRequestDurationMicroseconds.observe({
-          method: req.method,
-          route: req.route ? req.route.path : req.path,
-          code: res.statusCode
-      }, endTime-startTime);
-    });
-  
-  
-    next();
-  };
-  
-
-           // below code will add the request count and gauge to the monitoring
-// export const cleanupMiddleware = (req: Request, res: Response, next: NextFunction) => {
-//     const startTime = Date.now();
-//     activeRequestsGauge.inc();
-
-//     res.on('finish', function() {
-//         const endTime = Date.now();
-//         console.log(`Request took ${endTime - startTime}ms`);
-        
-//         requestCounter.inc({
-//             method: req.method,
-//             route: req.route ? req.route.path : req.path,
-//             status_code: res.statusCode
-//         });
-//         activeRequestsGauge.dec();
-//     });
-// }
-
