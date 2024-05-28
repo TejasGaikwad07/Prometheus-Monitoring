2923bfd16e105823d5771b0cb4300e0f6762e228
TejasGaikwad07 <tejasbharatgaikwad@gmail.com>
2024-05-28 20:46:37 +0530

    solved some errors

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,35 @@
 import express from "express";
-import { requestCountMiddleware } from "./Monitoring/requestCount";
+import { middleware } from "./middleware";
 import client from "prom-client";
+import { metricsMiddleware } from "./metrics";
+
 const app = express();
 
 app.use(express.json());
+app.use(middleware);
+app.use(metricsMiddleware);
 
-app.use(requestCountMiddleware);
+app.get("/user", async (req, res) => {
 
-app.get("/user", (req, res) => {
-  res.send({
-    name: "John Doe",
-    age: 25,
-  });
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    res.send({
+        name: "John Doe",
+        age: 25,
+    });
 });
 
 app.post("/user", (req, res) => {
-  const user = req.body;
-  res.send({
-    ...user,
-    id: 1,
-  });
+    const user = req.body;
+    res.send({
+        ...user,
+        id: 1,
+    });
 });
 
 app.get("/metrics", async (req, res) => {
-  const metrics = await client.register.metrics();
-  res.set("Content-Type", client.register.contentType);
-  res.end(metrics);
-});
+    const metrics = await client.register.metrics();
+    res.set('Content-Type', client.register.contentType);
+    res.end(metrics);
+})
 
 app.listen(3000);
