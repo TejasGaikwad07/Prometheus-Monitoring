ae0cd89a55835f3f8b0c31038d5f710b9e4fdd0c
TejasGaikwad07 <tejasbharatgaikwad@gmail.com>
2024-05-28 20:47:32 +0530

    formatting the code

--- src/metrics/index.ts
+++ src/metrics/index.ts
@@ -3,28 +3,34 @@ import { requestCounter } from "./requestCount";
 import { activeRequestsGauge } from "./activeRequests";
 import { httpRequestDurationMicroseconds } from "./requestTime";
 
-export const metricsMiddleware = (req: Request, res: Response, next: NextFunction) => {
-    const startTime = Date.now();
-    activeRequestsGauge.inc();
+export const metricsMiddleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const startTime = Date.now();
+  activeRequestsGauge.inc();
 
-    res.on('finish', function() {
-        const endTime = Date.now();
-        const duration = endTime - startTime;
-    
-        
-        requestCounter.inc({
-            method: req.method,
-            route: req.route ? req.route.path : req.path,
-            status_code: res.statusCode
-        });
+  res.on("finish", function () {
+    const endTime = Date.now();
+    const duration = endTime - startTime;
 
-        httpRequestDurationMicroseconds.observe({
-            method: req.method,
-            route: req.route ? req.route.path : req.path,
-            code: res.statusCode
-        }, duration);
-
-        activeRequestsGauge.dec();
+    requestCounter.inc({
+      method: req.method,
+      route: req.route ? req.route.path : req.path,
+      status_code: res.statusCode,
     });
-    next();
-}
+
+    httpRequestDurationMicroseconds.observe(
+      {
+        method: req.method,
+        route: req.route ? req.route.path : req.path,
+        code: res.statusCode,
+      },
+      duration
+    );
+
+    activeRequestsGauge.dec();
+  });
+  next();
+};
