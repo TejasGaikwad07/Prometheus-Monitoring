954f80aa757372f13d6b9a1f0d069c3c3f2d3248
TejasGaikwad07 <tejasbharatgaikwad@gmail.com>
2024-05-28 20:48:13 +0530

    formatting the code

--- src/metrics/requestCount.ts
+++ src/metrics/requestCount.ts
@@ -1,9 +1,8 @@
 import { NextFunction, Request, Response } from "express";
 import client from "prom-client";
 
-// Create a counter metric
 export const requestCounter = new client.Counter({
-    name: 'http_requests_total',
-    help: 'Total number of HTTP requests',
-    labelNames: ['method', 'route', 'status_code']
+  name: "http_requests_total",
+  help: "Total number of HTTP requests",
+  labelNames: ["method", "route", "status_code"],
 });
